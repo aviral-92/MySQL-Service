633af23179d83a8bea35c8bf495077bafbc40cd5
bp-stripa006 <Abermy21>
2016-05-24 18:18:03 +0530

    Controller Refactored

--- src/main/java/com/mySql/Controller/MySqlController.java
+++ src/main/java/com/mySql/Controller/MySqlController.java
@@ -1,8 +1,10 @@
 package com.mySql.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mySql.db.DbOperation;
@@ -13,16 +15,16 @@ public class MySqlController {
 	@Autowired
 	private DbOperation dbOperation;
 
-	@RequestMapping(value = "/createDatabase")
-	public void createDatabase() {
+	@RequestMapping(value = "/createDatabase", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String createDatabase() {
 		dbOperation.createDb();
-		System.out
-				.println("Created database name 'aviraldb' 2 tables student and employee with ");
-		System.out
-				.println("fields - (id(Auto-increment),name,age) in both and 10 rows also.");
+		String response = "Created database name 'aviraldb' 2 tables student and employee with fields - (id(Auto-increment),name,age) in both and 10 rows also.";
+		System.out.println(response);
+
+		return response;
 	}
 
-	@RequestMapping(value = "/getDataSource")
+	@RequestMapping(value = "/getDataSource", method = RequestMethod.HEAD)
 	public JdbcTemplate template() {
 		return dbOperation.templateDB();
 	}
